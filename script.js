51988d389b59efd29c7b0d231729e42894706998
thevishujadhav <vishuj802@gmail.com>
2024-05-28 01:03:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// gsap.from("#nav h3", {
+//   y: -50,
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.5,
+//   stagger: 0.5,
+// });
+
+// gsap.from("#main h1", {
+//   y: -50,
+//   opacity: 0,
+//   duration: 2,
+//   delay: 2,
+//   stagger: 1,
+// });
+
+var timeline = gsap.timeline();
+
+timeline.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.4,
+  stagger: 0.3,
+});
+
+timeline.from("#main h1", {
+    x: -500,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.4,
+  });
+  timeline.from("#main img", {
+    x: 100,
+    rotate: 45,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.2,
+    stagger: 0.8,
+  });
+  
+  timeline.from("#main h1", {
+    x: -500,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.2,
+    stagger: 0.5,
+  });
+  
+  timeline.from("#main footer h3", {
+    x: -500,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.2,
+    stagger: 0.8,
+  });
+  
+  timeline.from("#main footer p", {
+    x: -500,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.2,
+    stagger: 0.8,
+  });
